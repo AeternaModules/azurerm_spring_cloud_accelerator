32a9cfffb18b612175f95d058c622e1a08e977cd
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:15:12 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,12 @@ output "spring_cloud_accelerators" {
   description = "All spring_cloud_accelerator resources"
   value       = azurerm_spring_cloud_accelerator.spring_cloud_accelerators
 }
+output "spring_cloud_accelerators_name" {
+  description = "List of name values across all spring_cloud_accelerators"
+  value       = [for k, v in azurerm_spring_cloud_accelerator.spring_cloud_accelerators : v.name]
+}
+output "spring_cloud_accelerators_spring_cloud_service_id" {
+  description = "List of spring_cloud_service_id values across all spring_cloud_accelerators"
+  value       = [for k, v in azurerm_spring_cloud_accelerator.spring_cloud_accelerators : v.spring_cloud_service_id]
+}
 
