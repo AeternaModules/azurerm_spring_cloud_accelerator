4e6774330133557937914170083dbf2ea5a29fdb
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:30 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "spring_cloud_accelerators" {
-  description = "All spring_cloud_accelerator resources"
-  value       = azurerm_spring_cloud_accelerator.spring_cloud_accelerators
-}
 output "spring_cloud_accelerators_name" {
-  description = "List of name values across all spring_cloud_accelerators"
-  value       = [for k, v in azurerm_spring_cloud_accelerator.spring_cloud_accelerators : v.name]
+  description = "Map of name values across all spring_cloud_accelerators, keyed the same as var.spring_cloud_accelerators"
+  value       = { for k, v in azurerm_spring_cloud_accelerator.spring_cloud_accelerators : k => v.name }
 }
 output "spring_cloud_accelerators_spring_cloud_service_id" {
-  description = "List of spring_cloud_service_id values across all spring_cloud_accelerators"
-  value       = [for k, v in azurerm_spring_cloud_accelerator.spring_cloud_accelerators : v.spring_cloud_service_id]
+  description = "Map of spring_cloud_service_id values across all spring_cloud_accelerators, keyed the same as var.spring_cloud_accelerators"
+  value       = { for k, v in azurerm_spring_cloud_accelerator.spring_cloud_accelerators : k => v.spring_cloud_service_id }
 }
 
